caf37ba377851ec75a4728af853fc51570e99ac9
Ziya Xu <gracexuyaya@outlook.com>
2023-07-04 14:46:40 -0500

    Ziya&James 7.4 Implementation

--- Network.java
+++ Network.java
@@ -1,141 +1,34 @@
-import java.io.IOException;
-import java.util.*;
 import java.io.FileWriter;
-public class Network { //original: public class Network implements Iterable<Integer>
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 
-    //2D4n network always or change network later?
+public class Network { //original: public class Network implements Iterable<Integer>
     //another program that generate network and output a text file of network
     //PDG program read info from the text file -> a more efficient approach
     //Good for different kinds of network
     //format of network can be read by software such as (network bench, pajek)
     public ArrayList<Agent> agentsList;
     public int agentCount; //final int -> int for agentRemove() in PDG 06/28
-
+    public int aliveAgentCount;
     public int cooperatorCount;
-
-    public class Agent {
-
-        public int index;
-        public LinkedList<Agent> adjLists;
-        public double actualPayoffs;//agent's actualPayoffs in one trial
-        public boolean cooperate;
-
-        public Agent(int index) {
-            this.index = index;
-            adjLists = new LinkedList<>();
-            actualPayoffs = 0; //refresh before each trail
-            cooperate = false;  //initialize agent as defector, will reset them in functions in PlayPDG 06/28
-            //add initial num of neighbors, num of neighbors rn?
-
-        }
-
-        public int getIndex() {
-            return this.index;
-        }
-
-        public void setIndex(int index) {
-            this.index = index;
-        }
-
-        public boolean getCooperate() {
-            return this.cooperate;
-        }
-
-        public void setCooperate(boolean cooperate) {
-            this.cooperate = cooperate;
-
-
-        }
-
-        /**
-         * Returns a list of all the neighbors of agent v in the network
-         */
-        public LinkedList<Agent> getAdjLists() {
-            return adjLists;
-        }
-
-        @Override
-        public boolean equals(Object a) {
-            if (this == a) {
-                return true;
-            }
-
-            if (a.getClass() != this.getClass()) {
-                return false;
-            }
-
-            return this.index == ((Agent) a).index;
-        }
-
-        /**
-         * Function enable an agent to update its strategy
-         * Randomly choose a survived neighbor to imitate and update strategy
-         *
-         * @Return a boolean that mark the new strategy of the agent for the next trail
-         */
-        public boolean strategyUpdate() {
-            boolean result;
-            result = this.cooperate;
-
-            int neighborNum = adjLists.size();
-
-
-            int imiIndex = (int) Math.random() * neighborNum;//index of a randomly chosen neighbor
-            Agent imiNeighbor = adjLists.get(imiIndex);
-            double noise = 0.1;//constant value of uncertainty in assessing payoff
-            double Wij = 1 / (1 + Math.exp(-(imiNeighbor.actualPayoffs - this.actualPayoffs) / noise));
-            if (Math.random() < Wij) {
-                result = imiNeighbor.getCooperate();
-            }
-            return result;
-        }
-
-        //we can write strategy update for reinforcement learning function later.
-        //-also update the q table in each trail
-        //write beginning of code signify what learning approach will be used
-        //
-
-
-    }
-
-    public void strategyUpdateAll() {
-        for (int i = 0; i < agentCount; i++) {
-
-            Agent a = agentsList.get(i);
-            boolean originalCoop = a.cooperate;
-            a.setCooperate(a.strategyUpdate());
-            if(a.getCooperate()==false && originalCoop==true){
-                cooperatorCount--;
-            } else if (a.getCooperate()==true && originalCoop==false) {
-                cooperatorCount++;
-
-            }
-
-
-        }
-    }
+    //we need to calculate eliminatedPercent and defectorPercent after each trial using the above instance variables
 
     /**
      * Initializes a network with numAgents and no Edges.
      *
-     * @param numAgents       > 4
+     * @param numAgents > 4
      * @param defectorPercent
      */
     public Network(int numAgents, double defectorPercent) {
         agentsList = new ArrayList<Agent>();
-
-
-        ArrayList<Integer> number = new ArrayList<>();
-        for (int k = 0; k < numAgents; k++) {
-            number.add(k);
-        }
-        Collections.shuffle(number);
-
         for (int i = 0; i < numAgents; i++) {
-            agentsList.add(new Agent(number.get(i)));
+            agentsList.add(new Agent(i));
         }
 
         agentCount = numAgents;
+        aliveAgentCount = numAgents;
 
         int numDefect = (int) Math.round(numAgents * defectorPercent);
         cooperatorCount = numAgents - numDefect;
@@ -148,45 +41,30 @@ public class Network { //original: public class Network implements Iterable<Inte
         for (int i = 0; i < numAgents - numDefect; i++) {
             ifCooperate.add(true);
         }
-        Collections.shuffle(ifCooperate);
+        Collections.shuffle(ifCooperate); //shuffle the sequence of true and false
         System.out.println("if cooperate content:" + ifCooperate);
         for (int i = 0; i < agentCount; i++) {
             agentsList.get(i).setCooperate(ifCooperate.get(i));
         }
     }
 
-    /*public class Edge {
-
-        private final int from;
-        private final int to;
-
-
-
-        Edge(int from, int to) { //deleted instance variable, weight, 06/28
-            this.from = from;
-            this.to = to;
+    public void strategyUpdateAll() {
+        for (int i = 0; i < agentCount; i++) {
+            Agent a = agentsList.get(i);
+            boolean originalCoop = a.cooperate;
+            a.setCooperate(a.strategyUpdate());
+            if (a.getCooperate() == false && originalCoop == true) {
+                cooperatorCount--;
+            } else if (a.getCooperate() == true && originalCoop == false) {
+                cooperatorCount++;
+            }
         }
-
-        public int getFrom(){
-            return from;
-        }
-
-        public int getTo(){
-            return to;
-        }
-
-        public String toString() {
-            return "(" + from + ", " + to + ")";
-        }
-
-    }*/
-
+    }
 
     /**
      * Adds an edge (V1, V2) between two neighbors in the network.
+     * If the edge (V1, V2) already exist, return
      */
-    //If the Edge already exists, replaces the current Edge with a new Edge with. should we write about this case?
-    //06/28
     public void addEdge(Agent a1, Agent a2) {
         if (a1.adjLists.contains(a2)) {
             return;
@@ -197,8 +75,8 @@ public class Network { //original: public class Network implements Iterable<Inte
 
 
     /**
-     * Returns true if there exists an Edge from agent FROM to agent TO.
-     * Returns false otherwise.
+     * returns true if there exists an Edge from agent FROM to agent TO.
+     * returns false otherwise.
      */
     public boolean isAdjacent(Agent a1, Agent a2) {
         if (a1.equals(a2)) {
@@ -212,8 +90,9 @@ public class Network { //original: public class Network implements Iterable<Inte
         return false;
     }
 
-
-    //generate a 2D4N network with agentNum number of agents
+    /**
+     * generate a 2D4N network with agentNum number of agents
+     */
     public void generate2D4N() {
         for (int i = 0; i < agentCount; i++) {
             LinkedList<Agent> neighbors = agentsList.get(i).adjLists;
@@ -224,9 +103,12 @@ public class Network { //original: public class Network implements Iterable<Inte
         }
     }
 
-    public void printNetworkToFile() throws IOException {
-        FileWriter Network2D4N = new FileWriter("2D4N.txt");
-
+    /**
+     * print the given network to a file with filename in the format of Pajek net file
+     * @throws IOException if the file cannot be opened
+     */
+    public void printNetworkToFile(String filename) throws IOException {
+        FileWriter Network2D4N = new FileWriter(filename);
         int[][] adjMatrix = new int[agentCount][agentCount];
         Network2D4N.write(" ");
         for (int i = 0; i < agentCount; i++) {
@@ -251,115 +133,101 @@ public class Network { //original: public class Network implements Iterable<Inte
     public static void main(String[] args) throws IOException {
         Network N = new Network(10000, 0.2);
         N.generate2D4N();
-        N.printNetworkToFile();
+        N.printNetworkToFile("2D4N.txt");
+    }
+
+
+
+
+
+    public class Agent {
+
+        public int index;
+        public LinkedList<Agent> adjLists;
+        public double actualPayoffs;//agent's actualPayoffs in one trial
+        private boolean cooperate;
+        private boolean eliminated;
+
+        public Agent(int index) {
+            this.index = index;
+            adjLists = new LinkedList<>();
+            actualPayoffs = 0; //refresh before each trail
+            cooperate = false;  //initialize agent as defector, will reset them in functions in PlayPDG 06/28
+            eliminated = false;
+        }
+
+        public int getIndex() {
+            return this.index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public boolean getCooperate() {
+            return this.cooperate;
+        }
+
+        public void setCooperate(boolean cooperate) {
+            this.cooperate = cooperate;
+        }
+
+        public boolean getEliminated (){
+            return this.eliminated;
+        }
+
+        public void setEliminated(boolean eliminated){
+            this.eliminated = eliminated;
+        }
+
+        /**
+         * Returns a list of all the neighbors of agent v in the network
+         */
+        public LinkedList<Agent> getAdjLists() {
+            return adjLists;
+        }
+
+        public int neighborNum(){
+            return adjLists.size();
+        }
+
+        @Override
+        public boolean equals(Object a) {
+            if (this == a) {
+                return true;
+            }
+            if (a.getClass() != this.getClass()) {
+                return false;
+            }
+            return this.index == ((Agent) a).index;
+        }
+
+        /**
+         * Function enable an agent to update its strategy
+         * Randomly choose a survived neighbor to imitate and update strategy
+         *
+         * @Return a boolean that mark the new strategy of the agent for the next trail
+         */
+        public boolean strategyUpdate() {
+            boolean result = this.cooperate;
+            int neighborNum = adjLists.size();
+            int imiIndex = (int) Math.random() * neighborNum;//index of a randomly chosen neighbor
+            Agent imiNeighbor = adjLists.get(imiIndex);
+            double noise = 0.1;//constant value of uncertainty in assessing payoff
+            double Wij = 1 / (1 + Math.exp(-(imiNeighbor.actualPayoffs - this.actualPayoffs) / noise));
+            if (Math.random() < Wij) {
+                result = imiNeighbor.getCooperate();
+            }
+            return result;
+        }
+
+        //we can write strategy update for reinforcement learning function later.
+        //-also update the q table in each trail
+        //write beginning of code signify what learning approach will be used
+        //
+
+
     }
 }
 
 
-
-
-    /** Returns an Iterator that outputs the agents of the graph in topological
-       sorted order. */
-    /*
-    public Iterator<Integer> iterator() {
-        return new TopologicalIterator();
-    }
-
-     */
-
-
-    /* Returns the collected result of performing a depth-first search on this
-       network's agents starting from V. */
-    /*
-    public List<Integer> dfs(int v) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        Iterator<Integer> iter = new DFSIterator(v);
-
-        while (iter.hasNext()) {
-            result.add(iter.next());
-        }
-        return result;
-    }
-
-     */
-
-    /* Returns true iff there exists a path from START to STOP. Assumes both
-       START and STOP are in this network. If START == STOP, returns true. */
-    /*
-    public boolean pathExists(int start, int stop) {
-        return dfs(start).contains(stop);
-    }
-
-     */
-
-
-    /* Returns the path from START to STOP. If no path exists, returns an empty
-       List. If START == STOP, returns a List with START. */
-    /*
-    public List<Integer> path(int start, int stop) {
-        List<Integer> result = new ArrayList<Integer>();
-        if (start == stop){
-            result.add(start);
-        }else if (pathExists(start, stop)) {
-            List<Integer> path = dfs(start);
-            int i = path.indexOf(stop);
-            int pathNode= stop;
-            result.add(stop);
-            while (path.get(i) != start) {
-                if (isAdjacent(pathNode, path.get(i-1))||isAdjacent(path.get(i-1), pathNode)){
-                    result.add(path.get(i-1));
-                    pathNode = path.get(i-1);
-                }
-                i--;
-            }
-        }
-        List<Integer> temp = new ArrayList<>();
-        for (int i = result.size() -1; i >= 0; i--){
-            temp.add(result.get(i));
-        }
-        return temp;
-    }
-
-     */
-
-    /*
-    public List<Integer> topologicalSort() {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        Iterator<Integer> iter = new TopologicalIterator();
-        while (iter.hasNext()) {
-            result.add(iter.next());
-        }
-        return result;
-    }
-
-     */
-
-
-    /*
-    private void printDFS(int start) {
-        System.out.println("DFS traversal starting at " + start);
-        List<Integer> result = dfs(start);
-        Iterator<Integer> iter = result.iterator();
-        while (iter.hasNext()) {
-            System.out.println(iter.next() + " ");
-        }
-        System.out.println();
-        System.out.println();
-    }
-
-    private void printPath(int start, int end) {
-        System.out.println("Path from " + start + " to " + end);
-        List<Integer> result = path(start, end);
-        if (result.size() == 0) {
-            System.out.println("No path from " + start + " to " + end);
-            return;
-        }
-        Iterator<Integer> iter = result.iterator();
-        while (iter.hasNext()) {
-            System.out.println(iter.next() + " ");
-        }
-        System.out.println();
-        System.out.println();
-    }
-
-}*/
--- PlayPDG.java
+++ PlayPDG.java
@@ -25,13 +25,12 @@ public class PlayPDG {
     private PlayPDG(int agentNum, double T, double toleranceP,double defectorPercent) throws Exception {
         //use different tParameter as test cases
         if(agentNum<5) throw new Exception("Agent number must be large than 4");
-        N= new Network(agentNum,defectorPercent );
+        N= new Network(agentNum, defectorPercent);
         this.T = T;
-        tParameter=toleranceP; //added 06/28
-        //Reverse？
+        tParameter = toleranceP; //added 06/28
        // initializeNetwork(N, defectorPercent); //Initialize the network by make sure which agent cooperate&defect
         N.generate2D4N(); //Generate the 2D4n network
-        N.printNetworkToFile();
+        N.printNetworkToFile("2D4N.txt");
         int i = 1;
 
         while(N.agentCount != 0) {
@@ -43,7 +42,7 @@ public class PlayPDG {
             calculatePayoffsAll();
             agentRemoveAll();
             N.strategyUpdateAll();
-            N.printNetworkToFile();
+            N.printNetworkToFile("2D4N.txt");
             i++;
             System.out.println("Num of Coop: " + N.cooperatorCount);
 
@@ -65,23 +64,7 @@ public class PlayPDG {
      * @param N Total number of agents player wants to have when initialize the network
      * @param defectorPercent percentage of initial defector player wants to have in the network
      */
-    /*
-    private void initializeNetwork(Network N, double defectorPercent){
-        List<Boolean> ifCooperate = new ArrayList<Boolean>();
-        int numTrue = (int) Math.round(N.agentCount * defectorPercent);
-        for (int i = 0; i < numTrue; i++){
-            ifCooperate.add(true);
-        }
-        for (int i = 0; i < N.agentCount - numTrue; i++){
-            ifCooperate.add(false);
-        }
-        Collections.shuffle(ifCooperate);
-        for (int i = 0; i < N.agentCount; i++){
-          N.agentsList.get(i).setCooperate(ifCooperate.get(i));
-        }
-    }
 
-     */
 
     /**
      * Calculate payoff receive by an agent in one trail
@@ -127,60 +110,41 @@ public class PlayPDG {
      * @param index Agent's index in agentsList
      */
     public Network.Agent agentRemove(int index) throws Exception{
-        if(N.agentCount==0) throw new Exception("No agents in the network, can't remove agent. ");
-
         Network.Agent a = N.agentsList.get(index); //get out the agent
-        //System.out.println("threshold: " + tParameter*normalPayoff);
-
-        if (a.actualPayoffs < tParameter*normalPayoff ){
-
+        if (a.actualPayoffs < tParameter*normalPayoff && !a.getEliminated()){
             List<Network.Agent> neighbors = a.getAdjLists(); //get all agents' neighbor's index
             for (Network.Agent i: neighbors){
                 i.getAdjLists().remove(a);  //remove all connections between agent and its neighbor
             }
-            //N.agentsList.remove(a);
             return a;
         }
-
         return null;
-       // N.agentCount--;
     }
 
 
     public void agentRemoveAll() throws Exception {
-        //System.out.println("Original agentCount" + N.agentCount);
-        ArrayList<Network.Agent> removeList1 = new ArrayList<>();//store agents whose actualPayoffs < 0
-        ArrayList<Network.Agent> removeList2 = new ArrayList<>();//store agents w/o neighbors
-        for (int i=0; i<N.agentCount; i++){
-            //System.out.println("remove once");
-            removeList1.add(agentRemove(i));
-        }
-        for(int i=0; i< removeList1.size();i++){
-            N.agentsList.remove(removeList1.get(i));
-            Network.Agent a = removeList1.get(i);
-            if(a!=null) {
-                N.agentCount--;
-                if(a.getCooperate()) N.cooperatorCount--; //if b is a cooperator, update cooperatorCount
-            }
-
-        }
-        for(int i=0;i<N.agentCount;i++){
-            Network.Agent a =N.agentsList.get(i);
-            if(a.getAdjLists().size()==0){
-               removeList2.add(a);
-               // if(a.getCooperate()) N.cooperatorCount--;
+        if(N.aliveAgentCount==0) throw new Exception("No agents in the network, can't remove agent. ");
+        for (int i = 0; i < N.agentCount; i++){
+            Network.Agent a = N.agentsList.get(i);
+                if(agentRemove(i) != null){
+                    a.setEliminated(true);
+                    N.aliveAgentCount--;
+                    if (a.getCooperate()){
+                        N.cooperatorCount--;
+                }
             }
         }
-        for(int i=0; i< removeList2.size();i++){
-            Network.Agent a = removeList2.get(i);
-            N.agentsList.remove(a);
-            N.agentCount--;
-            if(a.getCooperate()) N.cooperatorCount--; //if b is a cooperator, update cooperatorCount
 
+        for (int i = 0; i < N.agentCount; i++){
+            Network.Agent a = N.agentsList.get(i);
+            if (a.neighborNum() == 0 && !a.getEliminated()){
+                a.setEliminated(true);
+                N.aliveAgentCount--;
+                if (a.getCooperate()){
+                    N.cooperatorCount--;
+                }
+            }
         }
-
-
-
         System.out.println("final agentCount" + N.agentCount);
     }
 
