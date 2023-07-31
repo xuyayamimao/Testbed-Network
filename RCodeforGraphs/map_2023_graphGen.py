9c1f71eff1810d6ba95d778a4349a5cd500c130b
Jia Chen <JiaChen@chenjiadeMacBook-Pro.local>
2023-07-31 17:08:13 -0500

    James 07/31 implementation

--- RCodeforGraphs/map_2023_graphGen.py
+++ RCodeforGraphs/map_2023_graphGen.py
@@ -5,47 +5,135 @@ import numpy as np
 import os
 
 
-
-#open txt file, need to change file path if used on window
-scoreFile = open(os.path.expanduser("~/Desktop/SummerMAP/test_pycharm_open.txt"), 'r')
-trialsFile = open(os.path.expanduser("~/Desktop/SummerMAP/test_pycharm_trials.txt"), 'r')
-scores = []
-trials = []
-for row in scoreFile:
-    row = row.split('\n')
-    if row[0] != '':
-        scores.append(float(row[0]))
-        print(scores)
-    else:
-        break
-
-for row in trialsFile:
-    row = row.split('\n')
-    if row[0] != '':
-        trials.append(int(row[0]))
-        print(trials)
-    else:
-        break
+def genPayoffPlot(actualTotalPayoffFilePath, ifAllCoopTotalPayoffFilePath, trialsFilePath,
+                  learning, alpha, b, hybrideOrNot):
+    #macbook way of opening file ***possibily also works on windows
+    actualPayOffFile = open(os.path.expanduser(actualTotalPayoffFilePath), 'r')
+    allCoopPayoffFile = open(os.path.expanduser(ifAllCoopTotalPayoffFilePath), 'r')
+    trialsFile = open(os.path.expanduser(trialsFilePath), 'r')
 
 
-plt.title("Scores and trials")
-plt.plot(trials, scores, 'g')
-#Convert values on y-axis into percentages, comment out this line
-#if we want to plot values of total payoffs into line plot
-#plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
-plt.xlabel('Trials')
-plt.ylabel('Scores')
-plt.legend(['What'])
+    actualPayoffs = []
+    allCoopPayoffs = []
+    trials = []
+
+    for row in actualPayOffFile:
+        row = row.split('\n')
+        if row[0] != '':
+            actualPayoffs.append(float(row[0]))
+            print(actualPayoffs)
+        else:
+            break
+
+    for row in allCoopPayoffFile:
+        row = row.split('\n')
+        if row[0] != '':
+            allCoopPayoffs.append(float(row[0]))
+            print(allCoopPayoffs)
+        else:
+            break
+
+    for row in trialsFile:
+        row = row.split('\n')
+        if row[0] != '':
+            trials.append(int(row[0]))
+            print(trials)
+        else:
+            break
+
+
+    plt.title(learning + "Agent " + "Alpha = " + alpha + " b = " + b + " " + hybrideOrNot + " totalPayoffs")
+    plt.plot(trials, actualPayoffs, 'g')
+    plt.plot(trials, allCoopPayoffs, 'r')
+    plt.xlabel('Trials')
+    plt.ylabel('Payoffs')
+    #Convert values on y-axis into percentages, comment out this line
+    #if we want to plot values of total payoffs into line plot
+    #plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    plt.legend(['Total payoffs', 'Total payoffs if all agents are cooperators'])
+    plt.show()
+
+    #add grid or not?
+
+    actualPayOffFile.close()
+    trialsFile.close()
+    allCoopPayoffFile.close()
+
+#If not in the combo mode, then set allAgBeRL = 0
+def CCCDDDPlot (CCFilePath, CDFilePath, DDFilePath, allAgBeRL, trialsFilePath,
+                  learning, alpha, b, hybrideOrNot):
+    # macbook way of opening file ***possibily also works on windows
+    CCFile = open(os.path.expanduser(CCFilePath), 'r')
+    CDFile = open(os.path.expanduser(CDFilePath), 'r')
+    DDFile = open(os.path.expanduser(DDFilePath), 'r')
+    trialsFile = open(os.path.expanduser(trialsFilePath), 'r')
+
+    CCPairs = []
+    CDPairs = []
+    DDPairs = []
+    trials = []
+
+    for row in CCFile:
+        row = row.split('\n')
+        if row[0] != '':
+            CCPairs.append(float(row[0]))
+            print(CCPairs)
+        else:
+            break
+
+    for row in CDFile:
+        row = row.split('\n')
+        if row[0] != '':
+            CDPairs.append(float(row[0]))
+            print(CDPairs)
+        else:
+            break
+
+    for row in DDFile:
+        row = row.split('\n')
+        if row[0] != '':
+            DDPairs.append(float(row[0]))
+            print(DDPairs)
+        else:
+            break
+
+    for row in trialsFile:
+        row = row.split('\n')
+        if row[0] != '':
+            trials.append(int(row[0]))
+            print(trials)
+        else:
+            break
+
+    plt.title(learning + "Agent " + "Alpha = " + alpha + " b = " + b + " " + hybrideOrNot + " C|C, C|D, D|D Pair Percentage")
+    plt.plot(trials, CCPairs, 'g')
+    plt.plot(trials, CDPairs, 'r')
+    plt.plot(trials, DDPairs, 'b')
+
+    #if the mode is in the combo mode
+    if allAgBeRL != 0:
+        plt.axvline(allAgBeRL, 'b', 'Trial number when all agents become RL agents')
+
+    plt.xlabel('Trials')
+    plt.ylabel('Percentage')
+    # Convert values on y-axis into percentages, comment out this line
+    # if we want to plot values of total payoffs into line plot
+    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    plt.legend(['C|C percentage', 'C|D percentage', 'D|D percentage'])
+    plt.show()
+
+
+# def aliveRLDumbPlot (aliveAgentPercentFilePath, RLAgentPercentFilePath, dumbAgentPercentFile, trialsFilePath,
+#                   learning, alpha, b, hybrideOrNot):
 
 
 
 
-plt.show()
 
-#add grid or not?
 
-scoreFile.close()
-trialsFile.close()
+
+
+
 
 
 
